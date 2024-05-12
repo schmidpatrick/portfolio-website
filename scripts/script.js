1095376e58b0fe505a1a698c5f75b34a2a4e2114
Patrick <schmidpatrick@outlook.com>
2024-05-12 21:32:27 +0200

    Add script

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+const getRandomRGB = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const rgb = `rgb(${r}, ${g}, ${b})`;
+
+    return rgb;
+};
+
+const greeting = () => {
+    const text = "Good Morning Y'all";
+    const element = document.querySelector('#greeting');
+
+    let output = '';
+
+    for (const char of text) {
+        if (char === ' ') {
+            output += '<br>';
+        } else {
+            const color = getRandomRGB();
+            output += `<span style="color:${color}">${char}</span>`;
+        }
+    }
+
+    element.innerHTML = output;
+};
+
+greeting();
+// setInterval(greeting, 100);
